157959a7b79cc8932d9b28d1fefd8609c066ea0c
Luis Henrique Gomes <l.henrique.b.g.filho@gmail.com>
2023-03-17 23:40:04 -0300

    Criado entidade item

--- src/domain/entities/Item.ts
+++ src/domain/entities/Item.ts
@@ -1,6 +1,47 @@
 import Product from './Product';
 
 export default class Item {
+  private _id: string;
+  private _product: Product;
+  private _quantity: number;
+
+  constructor(id: string, product: Product, quantity: number) {
+    this._id = id;
+    this._product = product;
+    this._quantity = quantity;
+  }
+
+  // getters
+  get id(): string {
+    return this._id;
+  }
+
+  get product(): Product {
+    return this._product;
+  }
+
+  get quantity(): number {
+    return this._quantity;
+  }
+
+  // setters
+  set id(id: string) {
+    this._id = id;
+  }
+
+  set product(product: Product) {
+    this._product = product;
+  }
+
+  set quantity(quantity: number) {
+    this._quantity = quantity;
+  }
+}
+
+
+/*import Product from './Product';
+
+export default class Item {
   id: string;
   product: Product;
   quantity: number;
@@ -12,4 +53,4 @@ export default class Item {
   }
 
   // getters e setters
-}
+}*/
